86e7e91dd41829beacdbd2b731c76f2025f34bcc
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-11 22:59:34 +0300

    first master commit

---
+++ Lab7.2(it).cpp
@@ -0,0 +1,84 @@
+﻿// Lab_7_2_1.cpp
+// Лабораторна робота № 7.2.1)
+// Опрацювання багатовимірних масивів ітераційними способами.
+// Варіант 17
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+void Create(int** a, const int k, const int n, const int Low, const int High);
+void Print(int** a, const int k, const int n);
+int SearchMaxInRow(int* row, const int n);
+int SearchMinInRow(int* row, const int n);
+void SwapMaxMin(int** a, const int k, const int n);
+
+int main() {
+    srand((unsigned)time(NULL));
+    int Low, High, n, k;
+
+    cout << "k = "; cin >> k;
+    cout << "n = "; cin >> n;
+    cout << "Low = "; cin >> Low;
+    cout << "High = "; cin >> High;
+
+    int** a = new int* [k];
+    for (int i = 0; i < k; i++)
+        a[i] = new int[n];
+
+    Create(a, k, n, Low, High);
+    Print(a, k, n);
+    SwapMaxMin(a, k, n);
+
+    cout << "Modified Matrix:" << endl;
+    Print(a, k, n);
+
+    for (int i = 0; i < k; i++)
+        delete[] a[i];
+    delete[] a;
+
+    return 0;
+}
+
+void Create(int** a, const int k, const int n, const int Low, const int High) {
+    for (int i = 0; i < k; i++)
+        for (int j = 0; j < n; j++)
+            a[i][j] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int** a, const int k, const int n) {
+    cout << endl;
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < n; j++)
+            cout << setw(4) << a[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int SearchMaxInRow(int* row, const int n) {
+    int maxIndex = 0;
+    for (int j = 1; j < n; j++) {
+        if (row[j] > row[maxIndex])
+            maxIndex = j;
+    }
+    return maxIndex;
+}
+int SearchMinInRow(int* row, const int n) {
+    int minIndex = 0;
+    for (int j = 1; j < n; j++) {
+        if (row[j] < row[minIndex])
+            minIndex = j;
+    }
+    return minIndex;
+}
+void SwapMaxMin(int** a, const int k, const int n) {
+    for (int i = 1; i < k; i += 2) {
+        int maxIndex = SearchMaxInRow(a[i], n);
+        int minIndex = SearchMinInRow(a[i - 1], n);
+        int temp = a[i][maxIndex];
+        a[i][maxIndex] = a[i - 1][minIndex];
+        a[i - 1][minIndex] = temp;
+        cout << "Swapped elements from row " << i << " and row " << i - 1 << endl << endl;
+    }
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab7.2(it).cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestSearchMaxInRow)
+		{
+			const int n = 5;
+			int row[n] = { 1, 3, 5, 2, 4 };
+			int expectedMaxIndex = 2;
+			int actualMaxIndex = SearchMaxInRow(row, n);
+			Assert::AreEqual(expectedMaxIndex, actualMaxIndex);
+		}
+		TEST_METHOD(TestSearchMinInRow)
+		{
+			const int n = 5;
+			int row[n] = { 6, 3, 8, 2, 7 };
+			int expectedMinIndex = 3;
+			int actualMinIndex = SearchMinInRow(row, n);
+			Assert::AreEqual(expectedMinIndex, actualMinIndex);
+		}
+		TEST_METHOD(TestSwapMaxMin)
+		{
+			const int k = 2;
+			const int n = 5;
+			int** a = new int* [k];
+			for (int i = 0; i < k; i++)
+				a[i] = new int[n];
+			int row1[n] = { 1, 2, 3, 4, 5 };
+			int row2[n] = { 10, 20, 30, 40, 50 };
+			for (int i = 0; i < n; i++) {
+				a[0][i] = row1[i];
+				a[1][i] = row2[i];
+			}
+			SwapMaxMin(a, k, n);
+			Assert::AreEqual(30, a[1][2]);
+			Assert::AreEqual(50, a[0][0]);
+			for (int i = 0; i < k; i++)
+				delete[] a[i];
+			delete[] a;
+		}
+	};
+}
